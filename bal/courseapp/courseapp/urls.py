4a1cc829c3a5b282b9aece4dc2181a727807fd06
sercan <sercan.20030@gmail.com>
2023-07-20 23:55:18 +0300

    commit

--- bal/courseapp/courseapp/urls.py
+++ bal/courseapp/courseapp/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.conf import settings#media yüklemesi için 
 from django.conf.urls.static import static#media yüklemesi için
-
 from django.contrib import admin
 from django.urls import path, include #burada dahil etme(include) ktüphanesini ekledik
-
+import statistics
+from django.conf import settings
 
 urlpatterns = [
     path('', include('bal.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
++ statistics(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
